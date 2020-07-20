22408c8777568ba132a5c1a110d2e6680edc2a86
Tau <tauoverpi@yandex.com>
2020-07-21 00:05:38 +0200

    Change to pixel

--- zig/splash.zig
+++ zig/splash.zig
@@ -1,8 +1,20 @@
 const std = @import("std");
 const time = std.time;
 
-var line = [_]u8{ 0x1b, '[', '4', '8', ';', '5', ';', '0', '0', '0', 'm', ' ' } ** 64 ++
-    [_]u8{ 0x1b, '[', '0', 'm', '\n' };
+var line = blk: {
+    @setEvalBranchQuota(10000);
+    const string = "\x1b[38;5;000m\x1b[48;5;000m▄" ** 64;
+    var buffer: [string.len]u8 = undefined;
+    for (string) |_, i| {
+        buffer[i] = string[i];
+    }
+    break :blk buffer;
+};
+
+test "" {
+    var bar = string ** 32 ++ [_]u8{};
+    std.debug.print("{}\n", .{bar});
+}
 
 const Machine = struct {
     t: u32,
@@ -15,7 +27,7 @@ const Machine = struct {
         const t = m.t;
         m.t +%= 1;
         const result = t *% t >> 16;
-        return @truncate(u4, result);
+        return @truncate(u8, result);
     }
 };
 
@@ -25,11 +37,25 @@ pub fn main() !void {
     var m = Machine.init();
 
     while (true) {
-        var offset: u32 = 7;
+        var offset: u32 = 18;
         var i: u32 = 64;
         while (i > 0) : ({
             i -= 1;
-            offset += 12;
+            offset += 25;
+        }) {
+            _ = std.fmt.formatIntBuf(
+                line[offset .. offset + 3],
+                m.step(),
+                10,
+                false,
+                .{ .alignment = .Right, .fill = '0', .width = 3 },
+            );
+        }
+        i = 64;
+        offset = 7;
+        while (i > 0) : ({
+            i -= 1;
+            offset += 25;
         }) {
             _ = std.fmt.formatIntBuf(
                 line[offset .. offset + 3],
@@ -40,6 +66,7 @@ pub fn main() !void {
             );
         }
         try stdout.writeAll(line[0..]);
-        time.sleep(time.ns_per_s / 16);
+        try stdout.print("\x1b[0m{}\n", .{m.t});
+        time.sleep(time.ns_per_s / 26);
     }
 }
