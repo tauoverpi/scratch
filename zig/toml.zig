8f6640a9ba52096bc7eb6032773200b5adc2e28a
Tau <tauoverpi@yandex.com>
2020-09-28 18:49:23 +0200

    Fix unicode handling

--- zig/toml.zig
+++ zig/toml.zig
@@ -6,14 +6,14 @@ const Allocator = std.mem.Allocator;
 
 const log = std.log.scoped(.@"toml parser");
 
-const ParserOptions = struct {
+pub const ParserOptions = struct {
     allocator: ?*Allocator = null,
     iterations: usize = max,
 
     const max = std.math.maxInt(usize);
 };
 
-const P = struct {
+pub const P = struct {
     text: []const u8,
     index: usize = 0,
     line: usize = 0,
@@ -24,28 +24,22 @@ const P = struct {
         if (p.index < p.text.len) {
             const byte = p.text[p.index];
             const len = try std.unicode.utf8ByteSequenceLength(byte);
-            if (len > 1) {
-                return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
-            } else return byte;
+            return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
         } else return null;
     }
 
     pub fn consume(p: *P) !u21 {
         if (try p.peek()) |byte| {
             const len = try std.unicode.utf8CodepointSequenceLength(byte);
-            if (len > 1) {
-                defer {
-                    for (p.text[p.index .. p.index + len]) |_| _ = p.consumeNoEof();
-                }
-                return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
+            defer {
+                for (p.text[p.index .. p.index + len]) |_| _ = p.consumeNoEof();
             }
-            return p.consumeNoEof();
+            return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
         }
         return error.UnexpectedEof;
     }
 
     fn consumeNoEof(p: *P) u8 {
-        std.debug.assert(p.index < p.text.len);
         const c = p.text[p.index];
         p.index += 1;
 
@@ -62,7 +56,7 @@ const P = struct {
     pub fn expect(p: *P, expected: u21) !void {
         if (try p.peek()) |char| {
             if (char != expected) return error.UnexpectedCharacter;
-            _ = p.consumeNoEof();
+            _ = try p.consume();
         } else return error.UnexpectedEof;
     }
 
@@ -81,6 +75,63 @@ const P = struct {
     pub fn eat(p: *P, expected: u21) bool {
         p.expect(expected) catch return false;
         return true;
+    }
+
+    pub fn range(p: *P, start: u21, end: u21) !u21 {
+        std.debug.assert(start < end);
+        if (try p.peek()) |codepoint| {
+            if (codepoint >= start and codepoint <= end) {
+                return try p.consume();
+            } else return error.UnexpectedCharacter;
+        }
+        return error.UnexpectedEof;
+    }
+
+    pub fn dec(p: *P) !u21 {
+        if (try p.peek()) |codepoint| {
+            switch (codepoint) {
+                '0'...'9' => return try p.consume(),
+                else => return error.UnexpectedCharacter,
+            }
+        }
+        return error.UnexpectedEof;
+    }
+
+    pub fn hex(p: *P) !u21 {
+        if (try p.peek()) |codepoint| {
+            switch (codepoint) {
+                '0'...'9', 'A'...'F', 'a'...'f' => return try p.consume(),
+                else => return error.UnexpectedCharacter,
+            }
+        }
+        return error.UnexpectedEof;
+    }
+
+    pub fn string(p: *P, pass: anytype) ![]const u8 {
+        const start = p.index;
+        var limit: usize = p.options.iterations;
+        while (pass(p)) {
+            if (limit == 0) return error.IterationLimitReached;
+            limit -= 1;
+        } else |e| switch (e) {
+            error.UnexpectedCharacter => break,
+            else => return e,
+        }
+        return p.text[start..p.index];
+    }
+
+    pub fn string1(p: *P, pass: anytype) ![]const u8 {
+        const start = p.index;
+        _ = try pass(p);
+        var limit: usize = p.options.iterations;
+        while (pass(p)) {
+            if (limit == 0) return error.IterationLimitReached;
+            limit -= 1;
+        } else |e| switch (e) {
+            error.UnexpectedCharacter => {},
+            else => return e,
+        }
+        return p.text[start..p.index];
     }
 };
 
@@ -158,7 +209,7 @@ pub const String = struct {
     }
 };
 
-fn string(p: *P) !String {
+fn inlineString(p: *P) !String {
     var escape = false;
     var len: usize = 0;
     var limit: usize = p.options.iterations;
@@ -190,7 +241,7 @@ test "string-plain" {
         .text =
         \\"this is a\n string"
     };
-    var result = try string(&p);
+    var result = try inlineString(&p);
     testing.expect(result.len + 1 == result.text.len);
     testing.expectEqualStrings("this is a\\n string", result.text);
     var buffer: [17]u8 = undefined;
@@ -201,7 +252,7 @@ test "string-plain" {
         .text =
         \\""
     };
-    var result2 = try string(&p);
+    var result2 = try inlineString(&p);
     testing.expect(result2.len == 0);
 }
 
@@ -282,7 +333,7 @@ test "string-multiline" {
     testing.expect(result2.len == 0);
 }
 
-fn literalString(p: *P) !String {
+fn inlineLiteralString(p: *P) !String {
     var limit: usize = p.options.iterations;
 
     try p.expect('`');
@@ -310,7 +361,7 @@ test "string-literal" {
         \\`this is a literal "with" """quotes""" and no escapes//\\`
     };
 
-    const result = try literalString(&p);
+    const result = try inlineLiteralString(&p);
     testing.expect(result.len == result.text.len);
     testing.expectEqualStrings(
         \\this is a literal "with" """quotes""" and no escapes//\\
@@ -355,4 +406,15 @@ test "string-multiline-literal" {
         \\this is a literal multi-line
         \\"with" """quotes""" and no'' ' escapes//\\
     , result.text);
+}
+
+// Keys
+
+fn key(p: *P) !String {
+    return inlineString(p) catch inlineLiteralString(p) catch |e| switch (e) {
+        error.UnexpectedCharacter => {
+            @panic("idhfh");
+        },
+        else => return e,
+    };
 }
