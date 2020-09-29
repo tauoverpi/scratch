e5d185686367cea0408d68a358e01ba32c766d03
Tau <tauoverpi@yandex.com>
2020-09-29 18:41:50 +0200

    Update to procedural style

--- zig/utf8-parser.zig
+++ zig/utf8-parser.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
 pub const ParserOptions = struct {
@@ -14,126 +15,203 @@ pub const P = struct {
     line: usize = 0,
     column: usize = 0,
     options: ParserOptions = .{},
+};
 
-    pub fn peek(p: P) !?u21 {
-        if (p.index < p.text.len) {
-            const byte = p.text[p.index];
-            const len = try std.unicode.utf8ByteSequenceLength(byte);
-            return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
-        } else return null;
+pub fn peek(p: *P) !?u21 {
+    if (p.index < p.text.len) {
+        const byte = p.text[p.index];
+        const len = try std.unicode.utf8ByteSequenceLength(byte);
+        return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
+    } else return null;
+}
+
+test "parser-peek" {
+    {
+        var p = P{ .text = "example" };
+        testing.expect((try peek(&p)).? == 'e');
     }
-
-    pub fn consume(p: *P) !u21 {
-        if (try p.peek()) |byte| {
-            const len = try std.unicode.utf8CodepointSequenceLength(byte);
-            defer {
-                for (p.text[p.index .. p.index + len]) |_| _ = p.consumeNoEof();
-            }
-            return try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
-        }
-        return error.UnexpectedEof;
+    {
+        var p = P{ .text = "" };
+        testing.expect((try peek(&p)) == null);
     }
+}
 
-    fn consumeNoEof(p: *P) u8 {
-        const c = p.text[p.index];
-        p.index += 1;
+pub fn consume(p: *P) !u21 {
+    if (try peek(p)) |byte| {
+        const len = try std.unicode.utf8CodepointSequenceLength(byte);
+        const result = try std.unicode.utf8Decode(p.text[p.index .. p.index + len]);
+        p.index += len;
 
-        if (c == '\n') {
+        if (result == '\n') {
             p.line += 1;
             p.column = 0;
         } else {
             p.column += 1;
         }
-
-        return c;
+        return result;
     }
+    return error.UnexpectedEof;
+}
 
-    pub fn expect(p: *P, expected: u21) !void {
-        if (try p.peek()) |char| {
-            if (char != expected) return error.UnexpectedCharacter;
-            _ = try p.consume();
-        } else return error.UnexpectedEof;
+test "parser-consume" {
+    {
+        var p = P{ .text = "example" };
+        testing.expect((try consume(&p)) == 'e');
     }
+    {
+        var p = P{ .text = "" };
+        testing.expectError(error.UnexpectedEof, consume(&p));
+    }
+}
 
-    pub fn exact(p: *P, expected: []const u8) !void {
-        if (p.text.len < p.index + expected.len) return error.UnexpectedEof;
-        if (std.mem.startsWith(u8, p.text[p.index..], expected)) {
-            for (expected) |_| _ = p.consumeNoEof();
+pub fn expect(p: *P, expected: u21) !void {
+    if (try peek(p)) |char| {
+        if (char != expected) return error.UnexpectedCharacter;
+        _ = try consume(p);
+    } else return error.UnexpectedEof;
+}
+
+test "parser-expect" {
+    {
+        var p = P{ .text = "example" };
+        try expect(&p, 'e');
+        testing.expect(p.index == 1);
+        testing.expect(p.column == 1);
+        testing.expect(p.line == 0);
+    }
+}
+
+pub fn exact(p: *P, expected: []const u8) !void {
+    if (p.text.len < p.index + expected.len) return error.UnexpectedEof;
+    if (std.mem.startsWith(u8, p.text[p.index..], expected)) {
+        var remains = expected.len;
+        while (remains > 0) {
+            remains -= try std.unicode.utf8ByteSequenceLength(p.text[p.index]);
+            _ = try consume(p);
+        }
+    } else return error.UnexpectedCharacter;
+}
+
+test "parser-exact" {
+    {
+        var p = P{ .text = "example" };
+        try exact(&p, "example");
+        testing.expect(p.index == 7);
+        testing.expect(p.column == 7);
+        testing.expect(p.line == 0);
+    }
+}
+
+pub fn newline(p: *P) !void {
+    expect(p, '\n') catch try exact(p, "\r\n");
+}
+
+test "parser-newline" {
+    {
+        var p = P{ .text = "\n" };
+        try newline(&p);
+        testing.expect(p.index == 1);
+    }
+}
+
+pub fn range(p: *P, start: u21, end: u21) !u21 {
+    std.debug.assert(start < end);
+    if (try peek(p)) |codepoint| {
+        if (codepoint >= start and codepoint <= end) {
+            return try consume(p);
         } else return error.UnexpectedCharacter;
     }
+    return error.UnexpectedEof;
+}
 
-    pub fn newline(p: *P) bool {
-        p.expect('\n') catch p.exact("\r\n") catch return false;
-        return true;
+test "parser-range" {
+    {
+        var p = P{ .text = "abc123" };
+        testing.expect((try range(&p, 'a', 'z')) == 'a');
     }
+}
 
-    pub fn eat(p: *P, expected: u21) bool {
-        p.expect(expected) catch return false;
-        return true;
-    }
-
-    pub fn range(p: *P, start: u21, end: u21) !u21 {
-        std.debug.assert(start < end);
-        if (try p.peek()) |codepoint| {
-            if (codepoint >= start and codepoint <= end) {
-                return try p.consume();
-            } else return error.UnexpectedCharacter;
+pub fn decimal(p: *P) !u21 {
+    if (try peek(p)) |codepoint| {
+        switch (codepoint) {
+            '0'...'9' => return try consume(p),
+            else => return error.UnexpectedCharacter,
         }
-        return error.UnexpectedEof;
     }
+    return error.UnexpectedEof;
+}
 
-    pub fn decimal(p: *P) !u21 {
-        if (try p.peek()) |codepoint| {
-            switch (codepoint) {
-                '0'...'9' => return try p.consume(),
-                else => return error.UnexpectedCharacter,
-            }
-        }
-        return error.UnexpectedEof;
+test "parser-decimal" {
+    {
+        var p = P{ .text = "12" };
+        testing.expect((try decimal(&p)) == '1');
     }
+}
 
-    pub fn hexadecimal(p: *P) !u21 {
-        if (try p.peek()) |codepoint| {
-            switch (codepoint) {
-                '0'...'9', 'A'...'F', 'a'...'f' => return try p.consume(),
-                else => return error.UnexpectedCharacter,
-            }
+pub fn hexadecimal(p: *P) !u21 {
+    if (try peek(p)) |codepoint| {
+        switch (codepoint) {
+            '0'...'9', 'A'...'F', 'a'...'f' => return try consume(p),
+            else => return error.UnexpectedCharacter,
         }
-        return error.UnexpectedEof;
     }
+    return error.UnexpectedEof;
+}
 
-    pub fn string(p: *P, pass: anytype) ![]const u8 {
-        const start = p.index;
-        var limit: usize = p.options.iterations;
-        while (pass(p)) {
-            if (limit == 0) return error.IterationLimitReached;
-            limit -= 1;
-        } else |e| switch (e) {
-            error.UnexpectedCharacter => {},
-            else => return e,
-        }
-        return p.text[start..p.index];
+test "parser-hexadecimal" {
+    {
+        var p = P{ .text = "a2" };
+        testing.expect((try hexadecimal(&p)) == 'a');
     }
+}
 
-    pub fn string1(p: *P, pass: anytype) ![]const u8 {
-        const start = p.index;
-        _ = try pass(p);
-        var limit: usize = p.options.iterations;
-        while (pass(p)) {
-            if (limit == 0) return error.IterationLimitReached;
-            limit -= 1;
-        } else |e| switch (e) {
-            error.UnexpectedCharacter => {},
-            else => return e,
-        }
-        return p.text[start..p.index];
+pub fn string(p: *P, pass: anytype) ![]const u8 {
+    const start = p.index;
+    var limit: usize = p.options.iterations;
+    while (pass(p)) {
+        if (limit == 0) return error.IterationLimitReached;
+        limit -= 1;
+    } else |e| switch (e) {
+        error.UnexpectedEof, error.UnexpectedCharacter => {},
+        else => return e,
     }
+    return p.text[start..p.index];
+}
 
-    pub fn not(p: *P, stop: u21) !u21 {
-        if (try p.peek()) |codepoint| {
-            if (codepoint == stop) return error.UnexpectedCharacter;
-            return try p.consume();
-        }
-        return error.UnexpectedEof;
+test "parser-string" {
+    {
+        var p = P{ .text = "1234567890" };
+        testing.expectEqualStrings("1234567890", try string(&p, decimal));
+        testing.expect(p.index == 10);
     }
-};
+    {
+        var p = P{ .text = "12345;67890" };
+        testing.expectEqualStrings("12345", try string(&p, decimal));
+        testing.expect(p.index == 5);
+    }
+}
+
+pub fn string1(p: *P, pass: anytype) ![]const u8 {
+    const start = p.index;
+    _ = try pass(p);
+    var limit: usize = p.options.iterations;
+    while (pass(p)) {
+        if (limit == 0) return error.IterationLimitReached;
+        limit -= 1;
+    } else |e| switch (e) {
+        error.UnexpectedEof, error.UnexpectedCharacter => {},
+        else => return e,
+    }
+    return p.text[start..p.index];
+}
+
+test "parser-string1" {
+    {
+        var p = P{ .text = "abcdef" };
+        testing.expectEqualStrings("abcdef", try string1(&p, hexadecimal));
+    }
+    {
+        var p = P{ .text = "" };
+        testing.expectError(error.UnexpectedEof, string1(&p, hexadecimal));
+    }
+}
