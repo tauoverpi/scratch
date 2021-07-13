79eb39e19be7281ae79fe5b27319f13660cb674c
Simon A. Nielsen Knights <tauoverpi@yandex.com>
2021-07-13 17:17:11 +0200

    Use ticks internally

--- zig/timerwheel.zig
+++ zig/timerwheel.zig
@@ -1,147 +1,147 @@
 const std = @import("std");
-const mem = std.mem;
+const math = std.math;
 const fmt = std.fmt;
 const testing = std.testing;
-const math = std.math;
-const Timer = std.time.Timer;
 
-const Wheel = struct {
-    timer: Timer,
-    wheels: [6][64]List = [_][64]List{[_]List{.{}} ** 64} ** 6,
-    indices: [6]u8 = [_]u8{0} ** 6,
+pub fn Wheel(
+    comptime resolution: comptime_int,
+    comptime wheels: comptime_int,
+) type {
+    return struct {
+        wheel: [wheels][64]List = [_][64]List{[_]List{.{}} ** 64} ** wheels,
+        index: [wheels]u8 = [_]u8{0} ** wheels,
 
-    const log = std.log.scoped(.wheel);
-    const ms = std.time.ns_per_ms;
+        const log = std.log.scoped(.wheel);
 
-    pub const List = std.SinglyLinkedList(Entry);
+        pub const List = std.SinglyLinkedList(Entry);
 
-    pub const Entry = struct {
-        frame: anyframe,
-        due: u64,
+        const Self = @This();
 
-        pub fn format(
-            entry: Entry,
-            comptime _: []const u8,
-            options: fmt.FormatOptions,
-            writer: anytype,
-        ) !void {
-            _ = options;
-            try writer.print("{}", .{fmt.fmtDuration(entry.due)});
+        pub const Entry = struct {
+            ticks: u64,
+            frame: anyframe,
+
+            pub fn format(
+                entry: Entry,
+                comptime _: []const u8,
+                options: fmt.FormatOptions,
+                writer: anytype,
+            ) !void {
+                _ = options;
+                try writer.print("[ticks: {}]", .{entry.ticks});
+            }
+        };
+
+        const power = blk: {
+            var table: [wheels]u64 = undefined;
+            for (table) |*level, i| level.* = math.pow(u64, 64, i);
+            break :blk table;
+        };
+
+        fn getLevel(ticks: u64) u8 {
+            for (power) |_, i| {
+                const p = ((wheels - 1) - i);
+                if (ticks >= power[p] - 1) return @intCast(u8, p);
+            }
+            unreachable;
+        }
+
+        fn getSlot(level: u8, ticks: u64) u8 {
+            return @intCast(u8, if (level == 0) ticks else ticks / (power[level] + 1));
+        }
+
+        pub fn sleep(self: *Self, offset: u64) void {
+            self.sleepTicks(offset / resolution);
+        }
+
+        pub fn sleepTicks(self: *Self, ticks: u64) void {
+            suspend {
+                const level = getLevel(ticks);
+                const slot = getSlot(level, ticks);
+                var entry: List.Node = .{
+                    .next = undefined,
+                    .data = .{
+                        .ticks = ticks,
+                        .frame = @frame(),
+                    },
+                };
+
+                log.debug("new entry level:{} slot:{} ticks:{}", .{
+                    level,
+                    slot,
+                    ticks,
+                });
+
+                self.wheel[level][slot].prepend(&entry);
+            }
+        }
+
+        pub fn tick(self: *Self) ?List {
+            var list: List = self.wheel[0][self.index[0]];
+            self.wheel[0][self.index[0]] = .{};
+
+            for (self.index) |*index, wheel| {
+                while (self.wheel[wheel][index.*].popFirst()) |node| {
+                    const ticks = node.data.ticks;
+                    if (ticks == 0) {
+                        list.prepend(node);
+                    } else {
+                        // reschedule
+
+                        const level = getLevel(ticks);
+                        const slot = getSlot(level, ticks);
+
+                        const reduction = power[level] * (slot + 1);
+                        const n_ticks = math.sub(u64, ticks, reduction) catch 0;
+                        log.debug("{} <- {}", .{ n_ticks, ticks });
+                        const n_level = getLevel(n_ticks);
+                        const n_slot = getSlot(n_level, n_ticks);
+
+                        node.data.ticks = n_ticks;
+
+                        log.debug("moved entry " ++
+                            "level:{} slot:{} ticks:{} -> " ++
+                            "level:{} slot:{} ticks:{}", .{
+                            level,   slot,   ticks,
+                            n_level, n_slot, n_ticks,
+                        });
+
+                        const w_slot = (n_slot + self.index[n_level]) & 63;
+                        self.wheel[n_level][w_slot].prepend(node);
+                    }
+                }
+
+                index.* += 1;
+                index.* &= 63;
+
+                if (index.* != 0) break;
+            }
+
+            return list;
         }
     };
-
-    pub fn sleep(self: *Wheel, due: u64) void {
-        suspend {
-            var entry: List.Node = .{
-                .next = undefined,
-                .data = .{
-                    .frame = @frame(),
-                    .due = due,
-                },
-            };
-
-            const p = position(due);
-            const slot = (p.slot + self.indices[p.level]) & 63;
-
-            log.debug(
-                "placing entry on wheel {} slot {} due {}",
-                .{ p.level, slot, fmt.fmtDuration(due) },
-            );
-
-            self.wheels[p.level][slot].prepend(&entry);
-        }
-    }
-
-    pub fn peek(self: *Wheel) ?u64 {
-        for (self.indices) |index, wheel| {
-            var slot = index + 1;
-
-            if (self.wheels[wheel][index].first) |node| {
-                return node.data.due;
-            }
-
-            while (slot != index) : (slot = (slot + 1) & 63) {
-                if (self.wheels[wheel][slot].first) |node| {
-                    return node.data.due;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    fn position(due: u64) struct { level: u8, slot: u8 } {
-        const level = if (due < ms) 0 else math.log(u64, 64, due / ms);
-        const slot = (due / ms / math.pow(u64, 64, level)) - 1;
-        return .{
-            .level = @intCast(u8, level),
-            .slot = @intCast(u8, slot),
-        };
-    }
-
-    fn tick(self: *Wheel) List {
-        const list = self.wheels[0][self.indices[0]];
-        self.wheels[0][self.indices[0]] = .{};
-
-        self.indices[0] += 1;
-        self.indices[0] &= 63;
-
-        if (self.indices[0] == 0) for (self.indices[1..]) |*index, lower| {
-            const wheel = lower + 1;
-            log.debug("turning wheel {}", .{wheel});
-
-            var old = self.wheels[wheel][index.*];
-            self.wheels[wheel][index.*] = .{};
-
-            while (old.popFirst()) |node| {
-                node.data.due /= 64;
-                const p = position(node.data.due);
-                log.debug(
-                    "moving entry from wheel {} to wheel {} slot {} due {}",
-                    .{ wheel, lower, p.slot, fmt.fmtDuration(node.data.due) },
-                );
-                self.wheels[lower][(self.indices[lower] + p.slot) % 64].prepend(node);
-            }
-
-            index.* += 1;
-            index.* &= 63;
-
-            if (self.indices[wheel] != 0) break;
-        };
-
-        return list;
-    }
-};
-
-const example = struct {
-    pub fn e(w: *Wheel) void {
-        w.sleep(std.time.ns_per_ms * 64);
-    }
-
-    pub fn v(w: *Wheel) void {
-        w.sleep(std.time.ns_per_ms);
-    }
-
-    pub fn t(w: *Wheel) void {
-        w.sleep(std.time.ns_per_ms * 63 + std.time.ns_per_ms * math.pow(u64, 64, 5));
-    }
-};
+}
 
 test {
     testing.log_level = .debug;
+    const W = Wheel(std.time.ns_per_ms, 6);
+    const example = struct {
+        pub fn ms(w: *W, n: u64, p: u64, r: u64) void {
+            w.sleep(std.time.ns_per_ms * n * math.pow(u64, 64, p) + r);
+        }
+    };
 
-    var w: Wheel = .{ .timer = try Timer.start() };
-    _ = async example.e(&w);
-    _ = async example.v(&w);
-    _ = async example.t(&w);
+    var wheel: W = .{};
+    const ms = std.time.ns_per_ms;
 
-    var i: u32 = 0;
-    std.log.debug("{}", .{fmt.fmtDuration(w.peek().?)});
-    std.log.debug("{}", .{w.tick()});
-    std.log.debug("{}", .{fmt.fmtDuration(w.peek().?)});
-    while (i < 62) : (i += 1) _ = w.tick();
+    _ = async example.ms(&wheel, 3, 0, 0);
+    _ = async example.ms(&wheel, 3, 1, 0);
+    _ = async example.ms(&wheel, 3, 2, ms * 3);
+    _ = async example.ms(&wheel, 1, 2, 0);
 
-    std.log.debug("{}", .{w.tick()});
-    std.log.debug("{}", .{w.tick()});
-    std.log.debug("{}", .{fmt.fmtDuration(w.peek().?)});
+    std.log.debug("{}", .{wheel.tick()});
+    std.log.debug("{}", .{wheel.tick()});
+    std.log.debug("{}", .{wheel.tick()});
+    std.log.debug("{}", .{wheel.tick()});
+    std.log.debug("{}", .{wheel.tick()});
 }
