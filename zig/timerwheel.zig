703da31f028c800395385be87327e10889eab76e
Simon A. Nielsen Knights <tauoverpi@yandex.com>
2021-07-14 07:47:38 +0200

    Fix scheduling, still suffers from jumps

--- zig/timerwheel.zig
+++ zig/timerwheel.zig
@@ -3,8 +3,8 @@ const math = std.math;
 const fmt = std.fmt;
 const testing = std.testing;
 
-pub fn Wheel(
-    comptime resolution: comptime_int,
+pub fn TimerWheel(
+    comptime res: comptime_int,
     comptime wheels: comptime_int,
 ) type {
     return struct {
@@ -13,7 +13,9 @@ pub fn Wheel(
 
         const log = std.log.scoped(.wheel);
 
-        pub const List = std.SinglyLinkedList(Entry);
+        pub const List = std.TailQueue(Entry);
+
+        pub const resolution = res;
 
         const Self = @This();
 
@@ -47,7 +49,12 @@ pub fn Wheel(
         }
 
         fn getSlot(level: u8, ticks: u64) u8 {
-            return @intCast(u8, if (level == 0) ticks else ticks / (power[level] + 1));
+            if (level == 0) {
+                return @intCast(u8, ticks);
+            } else {
+                const slot = (ticks / power[level]);
+                return @intCast(u8, slot);
+            }
         }
 
         pub fn sleep(self: *Self, offset: u64) void {
@@ -58,6 +65,7 @@ pub fn Wheel(
             suspend {
                 const level = getLevel(ticks);
                 const slot = getSlot(level, ticks);
+                const w_slot = (slot + self.index[level]) & 63;
                 var entry: List.Node = .{
                     .next = undefined,
                     .data = .{
@@ -66,17 +74,18 @@ pub fn Wheel(
                     },
                 };
 
-                log.debug("new entry level:{} slot:{} ticks:{}", .{
+                log.debug("new entry level:{} slot:{} ticks:{} ({})", .{
                     level,
                     slot,
                     ticks,
+                    fmt.fmtDuration(ticks * resolution),
                 });
 
-                self.wheel[level][slot].prepend(&entry);
+                self.wheel[level][w_slot].append(&entry);
             }
         }
 
-        pub fn tick(self: *Self) ?List {
+        pub fn tick(self: *Self) List {
             var list: List = self.wheel[0][self.index[0]];
             self.wheel[0][self.index[0]] = .{};
 
@@ -84,7 +93,7 @@ pub fn Wheel(
                 while (self.wheel[wheel][index.*].popFirst()) |node| {
                     const ticks = node.data.ticks;
                     if (ticks == 0) {
-                        list.prepend(node);
+                        list.append(node);
                     } else {
                         // reschedule
 
@@ -93,20 +102,21 @@ pub fn Wheel(
 
                         const reduction = power[level] * (slot + 1);
                         const n_ticks = math.sub(u64, ticks, reduction) catch 0;
-                        log.debug("{} <- {}", .{ n_ticks, ticks });
+                        log.debug("{} <- {} ({})", .{ n_ticks, ticks, reduction });
                         const n_level = getLevel(n_ticks);
                         const n_slot = getSlot(n_level, n_ticks);
+                        const w_slot = (n_slot + self.index[n_level]) & 63;
 
                         node.data.ticks = n_ticks;
 
                         log.debug("moved entry " ++
                             "level:{} slot:{} ticks:{} -> " ++
-                            "level:{} slot:{} ticks:{}", .{
-                            level,   slot,   ticks,
-                            n_level, n_slot, n_ticks,
+                            "level:{} slot:{} ticks:{} ({}:{})", .{
+                            level,               slot,   ticks,
+                            n_level,             n_slot, n_ticks,
+                            self.index[n_level], w_slot,
                         });
 
-                        const w_slot = (n_slot + self.index[n_level]) & 63;
                         self.wheel[n_level][w_slot].prepend(node);
                     }
                 }
@@ -124,7 +134,7 @@ pub fn Wheel(
 
 test {
     testing.log_level = .debug;
-    const W = Wheel(std.time.ns_per_ms, 6);
+    const W = TimerWheel(std.time.ns_per_ms, 6);
     const example = struct {
         pub fn ms(w: *W, n: u64, p: u64, r: u64) void {
             w.sleep(std.time.ns_per_ms * n * math.pow(u64, 64, p) + r);
@@ -144,4 +154,17 @@ test {
     std.log.debug("{}", .{wheel.tick()});
     std.log.debug("{}", .{wheel.tick()});
     std.log.debug("{}", .{wheel.tick()});
+
+    for (W.power) |p| {
+        var i: u64 = 0;
+        while (i < 64) : (i += 1) {
+            const level = W.getLevel((i + 1) * p);
+            const slot = W.getSlot(level, (i + 1) * p);
+            std.log.debug("{}:{} {}", .{
+                level,
+                slot,
+                fmt.fmtDuration((i + 1) * p * ms),
+            });
+        }
+    }
 }
